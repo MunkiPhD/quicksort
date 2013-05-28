a730ea962c526eb41bab507fdf93b162c8844685
MunkiPhD <munkiphd@gmail.com>
2013-05-28 19:36:02 -0400

    added tests

---
+++ quicksort_spec.rb
@@ -0,0 +1,39 @@
+require_relative 'quicksort'
+
+describe QuickSort do
+	@qs = QuickSort.new
+
+	before :each do
+		@array = [5, 4, 1, 7, 8, 2, 2, 0, 1, 13, 5]
+		@sorted = @array.sort
+	end
+
+	context 'iterative' do
+		it 'returns the array if length is 0' do
+			expect(@qs.iterative([])).to eq []
+		end
+
+		it 'returns the array if length is 1' do
+			expect(@qs.iterative([1])).to eq [1]
+		end
+
+		it 'returns the array sorted' do
+			expect(@qs.iterative(@array)).to eq @sorted
+		end
+	end
+
+	context 'recursive' do
+		it 'returns the array if length is 0' do
+			expect(@qs.recursive([])).to eq []
+		end
+
+		it 'returns the array if length is 1' do
+			expect(@qs.recursive([1])).to eq [1]
+		end
+
+		it 'returns the array sorted' do
+			expect(@qs.recursive(@array)).to eq @sorted
+		end
+
+	end
+end
